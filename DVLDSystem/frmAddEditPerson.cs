cefc860b94f8efbd72339bff438a6cfdaa79089e
Mustafa A. Ibrahim <MustafaIbrahimGitHub990@gmail.com>
2025-02-28 21:31:15 +0300

    Project-17 _ DVLDSystem

---
+++ DVLDSystem/frmAddEditPerson.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLDSystem
+{
+    public partial class frmAddEditPerson : Form
+    {
+        public frmAddEditPerson()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAddEditPerson_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
