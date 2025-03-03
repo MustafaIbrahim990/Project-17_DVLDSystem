e50669787ac0a8e4b6cb9fac3c2054fed8a64d18
Mustafa A. Ibrahim <MustafaIbrahimGitHub990@gmail.com>
2025-03-03 23:06:48 +0300

    Add New Feature/ManagePeople/ctrlPersonInfoWithFilter

--- DVLDSystem/FormTest.Designer.cs
+++ DVLDSystem/FormTest.Designer.cs
@@ -28,23 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.ctrlPersonInfo1 = new DVLDSystem.DVLD.People.Controls.ctrlPersonInfo();
+            this.ctrlPersonInfoWithFilter1 = new DVLDSystem.DVLD.People.Controls.ctrlPersonInfoWithFilter();
             this.SuspendLayout();
             // 
-            // ctrlPersonInfo1
+            // ctrlPersonInfoWithFilter1
             // 
-            this.ctrlPersonInfo1.BackColor = System.Drawing.Color.White;
-            this.ctrlPersonInfo1.Location = new System.Drawing.Point(46, 36);
-            this.ctrlPersonInfo1.Name = "ctrlPersonInfo1";
-            this.ctrlPersonInfo1.Size = new System.Drawing.Size(1170, 439);
-            this.ctrlPersonInfo1.TabIndex = 0;
+            this.ctrlPersonInfoWithFilter1.BackColor = System.Drawing.Color.White;
+            this.ctrlPersonInfoWithFilter1.btnAddNewPersonEnabled = true;
+            this.ctrlPersonInfoWithFilter1.FilterPersonEnabled = true;
+            this.ctrlPersonInfoWithFilter1.Location = new System.Drawing.Point(77, 63);
+            this.ctrlPersonInfoWithFilter1.Name = "ctrlPersonInfoWithFilter1";
+            this.ctrlPersonInfoWithFilter1.Size = new System.Drawing.Size(1183, 551);
+            this.ctrlPersonInfoWithFilter1.TabIndex = 0;
             // 
             // FormTest
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1313, 702);
-            this.Controls.Add(this.ctrlPersonInfo1);
+            this.Controls.Add(this.ctrlPersonInfoWithFilter1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "FormTest";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -56,6 +58,6 @@
 
         #endregion
 
-        private DVLD.People.Controls.ctrlPersonInfo ctrlPersonInfo1;
+        private DVLD.People.Controls.ctrlPersonInfoWithFilter ctrlPersonInfoWithFilter1;
     }
 }
