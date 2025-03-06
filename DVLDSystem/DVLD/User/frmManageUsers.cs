81481228c926b7a271e636d4bbfa5c3273cbdbd2
Mustafa A. Ibrahim <MustafaIbrahimGitHub990@gmail.com>
2025-03-07 02:59:59 +0300

    Add New Feature/ManageUsers

---
+++ DVLDSystem/DVLD/User/frmManageUsers.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DVLDSystem_BusinessLayer;
+using DVLDSystem.Gobal_Classes;
+
+namespace DVLDSystem.DVLD.User
+{
+    public partial class frmManageUsers : Form
+    {
+        //Private Prpperties :-
+        private static DataTable _dtUserLists;
+
+
+        //Private Methods :-
+        private bool _IsVisible(Control control)
+        {
+            return control.Visible;
+        }
+
+        private void _FocusControl(Control control)
+        {
+            control.Focus();
+        }
+
+        private void _GetUserLists()
+        {
+            _dtUserLists = clsUser.GetAllUsers();
+        }
+
+        private void _ShowUserListsInGrid()
+        {
+            dgvUserLists.DataSource = _dtUserLists;
+        }
+
+        private void _ShowRecordCountInGrid()
+        {
+            lblRecordCount.Text = dgvUserLists.Rows.Count.ToString();
+        }
+
+        private void _RefreshDataInGrid()
+        {
+            _GetUserLists();
+            _ShowUserListsInGrid();
+            _ShowRecordCountInGrid();
+        }
+
+        private void _FilterDataInGridByIntValue(string ColumnName, int Value)
+        {
+            _dtUserLists.DefaultView.RowFilter = String.Format($"{ColumnName} = {Value}");
+        }
+
+        private void _FilterDataInGridByStringValue(string ColumnName, string Value)
+        {
+            _dtUserLists.DefaultView.RowFilter = String.Format($"{ColumnName} Like '{Value}%'");
+        }
+
+        private void _FilterDataInGridByBooleanValue(string ColumnName, bool Value)
+        {
+            _dtUserLists.DefaultView.RowFilter = String.Format($"{ColumnName} = {Value}");
+        }
+
+        private void _RefreshDataInGridWithoutFilter()
+        {
+            _dtUserLists.DefaultView.RowFilter = null;
+        }
+
+        private void _RefreshDataInGridWithFilter()
+        {
+            string FilterColumn = "";
+
+            switch (cbFilterBy.Text)
+            {
+                case "User ID":
+                    FilterColumn = "UserID";
+                    break;
+
+                case "Person ID":
+                    FilterColumn = "PersonID";
+                    break;
+
+                case "Full Name":
+                    FilterColumn = "FullName";
+                    break;
+
+                case "User Name":
+                    FilterColumn = "UserName";
+                    break;
+
+                default:
+                    FilterColumn = "None";
+                    break;
+            }
+
+            //We Filter PeopleListsInGrid :-
+            if (FilterColumn == "UserID" || FilterColumn == "PersonID")
+            {
+                _FilterDataInGridByIntValue(FilterColumn, int.Parse(txtFilterValue.Text.Trim()));
+            }
+            else
+            {
+                _FilterDataInGridByStringValue(FilterColumn, txtFilterValue.Text.Trim());
+            }
+            _ShowRecordCountInGrid();
+        }
+
+        private void _ResetColumnsInGrid()
+        {
+            if (dgvUserLists.Rows.Count > 0)
+            {
+                dgvUserLists.Columns[0].HeaderText = "User ID";
+                dgvUserLists.Columns[0].Width = 50;
+
+                dgvUserLists.Columns[1].HeaderText = "Person ID";
+                dgvUserLists.Columns[1].Width = 50;
+
+                dgvUserLists.Columns[2].HeaderText = "Full Name";
+                dgvUserLists.Columns[2].Width = 300;
+
+                dgvUserLists.Columns[3].HeaderText = "User Name";
+                dgvUserLists.Columns[3].Width = 150;
+
+                dgvUserLists.Columns[4].HeaderText = "Is Active";
+                dgvUserLists.Columns[4].Width = 110;
+            }
+        }
+
+
+        //Constructor :-
+        public frmManageUsers()
+        {
+            InitializeComponent();
+        }
+        private void frmManageUsers_Load(object sender, EventArgs e)
+        {
+            _RefreshDataInGrid();
+            cbFilterBy.SelectedIndex = 0;
+
+            _ResetColumnsInGrid();
+        }
+
+
+        //Filter By :-
+        private void cbFilterBy_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtFilterValue.Text = null;
+            cbIsActive.Text = null;
+            txtFilterValue.Visible = (cbFilterBy.Text != "None" && cbFilterBy.Text != "Is Active");
+            cbIsActive.Visible = (cbFilterBy.Text != "None" && cbFilterBy.Text == "Is Active");
+
+            if (cbFilterBy.Text == "None")
+            {
+                _RefreshDataInGridWithoutFilter();
+                _ShowRecordCountInGrid();
+                return;
+            }
+
+            if (_IsVisible(txtFilterValue))
+            {
+                _FocusControl(txtFilterValue);
+                return;
+            }
+
+            if (_IsVisible(cbIsActive))
+            {
+                cbIsActive.SelectedIndex = 0;
+                _FocusControl(cbIsActive);
+            }
+        }
+
+
+        //Is Active :-
+        private void cbIsActive_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbIsActive.Text == "All")
+            {
+                _RefreshDataInGridWithoutFilter();
+                _ShowRecordCountInGrid();
+                return;
+            }
+
+            if (cbIsActive.Text == "Yes")
+            {
+                _FilterDataInGridByBooleanValue("IsActive", true);
+            }
+            else
+            {
+                _FilterDataInGridByBooleanValue("IsActive", false);
+            }
+            _ShowRecordCountInGrid();
+        }
+
+
+        //Filter Value :-
+        private void txtFilterValue_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (cbFilterBy.Text == "User ID" || cbFilterBy.Text == "Person ID")
+                e.Handled = !char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar);
+        }
+        private void txtFilterValue_TextChanged(object sender, EventArgs e)
+        {
+            if (clsValidation.IsEmpty(txtFilterValue.Text.Trim()))
+            {
+                txtFilterValue.Text = null;
+                _RefreshDataInGridWithoutFilter();
+            }
+            else
+            {
+                _RefreshDataInGridWithFilter();
+            }
+            _ShowRecordCountInGrid();
+        }
+
+
+        //Close Form :-
+        private void btnCloseForm_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        //Add New User :-
+        private void btnAddNewUser_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //Show User Details(Info) :-
+        private void showDetailsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void dgvUserLists_DoubleClick(object sender, EventArgs e)
+        {
+            showDetailsToolStripMenuItem.PerformClick();
+        }
+
+
+        //Add New User :-
+        private void addNewPersonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            btnAddNewUser.PerformClick();
+        }
+
+
+        //Edit User :-
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //Delete User :-
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //Change User PassWord :-
+        private void ChangePassWordToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //Send Email to User :-
+        private void SendEmailToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This Feature is Not Implemented Yet!", "Not Ready!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+
+        //Phone Call to User :-
+        private void phoneCallToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This Feature is Not Implemented Yet!", "Not Ready!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
